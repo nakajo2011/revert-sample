a12d22266199bb3652558fdac0ad847a0ce9f6fe
Yukishige Nakajo <nakajo@haw.co.jp>
2017-12-26 17:33:15 +0900

    shape up.

--- contracts/RevertSample.sol
+++ contracts/RevertSample.sol
@@ -13,19 +13,11 @@ contract RevertSample {
   }
 
   function hoge() public {
-    bytes4 sig = bytes4(keccak256("countup()"));
-    address addr = lib;
-    bool res = false;
     string memory resmsg = "";
-    assembly {
-    let freep := mload(0x40)
-    let nextp := add(freep, 0x20) // 32byte 空ける
-    mstore(0x40, nextp) // free memoryの場所を更新
-    mstore(freep, mul(div(sig, 0x0100000000000000000000000000000000000000000000000000000000), 0x0100000000000000000000000000000000000000000000000000000000))
-    res := call(sub(gas, 100000), addr, 0, freep, 0x04, freep, 0x40)
-    returndatacopy(resmsg, 0x0, returndatasize)
-    }
-    if (!res) {
+    if (!lib.call(bytes4(keccak256("countup()")))) {
+      assembly {
+      returndatacopy(resmsg, 0x0, returndatasize)
+      }
       message = resmsg;
     }
   }
